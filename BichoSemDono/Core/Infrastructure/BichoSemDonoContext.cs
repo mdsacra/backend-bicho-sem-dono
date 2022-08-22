86d73937affa7b442cabcd8b95c52a18cafcdea7
Matheus Sacramento <matheus.sacramento@qualyteam.com.br>
2022-08-21 22:22:43 -0300

    Create DbContext and configure OwnerlessPetPostEntity

---
+++ BichoSemDono/Core/Infrastructure/BichoSemDonoContext.cs
@@ -0,0 +1,42 @@
+﻿using BichoSemDono.Domain.Post.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BichoSemDono.Core.Infrastructure;
+
+public class BichoSemDonoContext : DbContext
+{
+    public BichoSemDonoContext(DbContextOptions<BichoSemDonoContext> options) : base(options) { }
+
+    public DbSet<OwnerlessPetPost> OwnerlessPetPosts => Set<OwnerlessPetPost>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ConfigureOwnerlessPetPostEntity(modelBuilder);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    private void ConfigureOwnerlessPetPostEntity(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OwnerlessPetPost>()
+            .HasKey(opp => opp.Id);
+            
+        modelBuilder.Entity<OwnerlessPetPost>()
+            .Property(opp => opp.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Entity<OwnerlessPetPost>()
+            .Property(opp => opp.Description)
+            .IsRequired();
+        
+        modelBuilder.Entity<OwnerlessPetPost>()
+            .Property(opp => opp.CreatedAt)
+            .ValueGeneratedNever();
+
+        modelBuilder.Entity<OwnerlessPetPost>()
+            .OwnsOne(opp => opp.Localization);
+
+        modelBuilder.Entity<OwnerlessPetPost>()
+            .Property(opp => opp.PetSpecies)
+            .IsRequired();
+    }
+}
