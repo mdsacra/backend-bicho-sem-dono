8e5461b8160222deeee4d4313c256b4be3ff6f58
Matheus Sacramento <matheus.sacramento@qualyteam.com.br>
2022-08-21 23:29:48 -0300

    Add first migration that created OwnerlessPetPosts table

---
+++ BichoSemDono/Migrations/20220822020340_CreateOwnerlessPetPostsTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BichoSemDono.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOwnerlessPetPostsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OwnerlessPetPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PetSpecies = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FinishedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Localization_Latitude = table.Column<string>(type: "text", nullable: false),
+                    Localization_Longitude = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OwnerlessPetPosts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OwnerlessPetPosts");
+        }
+    }
+}
