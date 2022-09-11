dc8bd8927e1e756791cbc631484e04ca36dcb3ac
Matheus Sacramento <matheus.sacramento@qualyteam.com.br>
2022-09-10 21:51:11 -0300

    Create OwnerlessPetPostController

---
+++ BichoSemDono/Controllers/OwnerlessPetPostController.cs
@@ -0,0 +1,26 @@
+using BichoSemDono.Application.Post.CommandSide.Commands;
+using BichoSemDono.Application.Post.QuerySide.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BichoSemDono.Controllers;
+
+[ApiController]
+[Route("api/ownerless-pet-post")]
+public class OwnerlessPetPostController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public OwnerlessPetPostController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Guid>>> ListOwnerlesPetPosts()
+        => Ok(await _mediator.Send(new ListOwnerlessPetPostsQuery()));
+    
+    [HttpPost]
+    public async Task<ActionResult<Guid>> CreateOwnerlessPetPost([FromBody] CreateOwnerlessPetPostCommand command)
+        => Ok(await _mediator.Send(command));
+}
