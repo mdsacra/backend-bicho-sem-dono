7eea631ace963a9d8ff0d321b592b5594df6085f
Matheus Sacramento <matheus.sacramento@qualyteam.com.br>
2022-09-25 21:15:41 -0300

    Add migration to add address localization column

--- BichoSemDono/Migrations/20220926001342_AddAdressLocalizationColumn.Designer.cs
+++ BichoSemDono/Migrations/20220926001342_AddAdressLocalizationColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BichoSemDono.Core.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BichoSemDono.Migrations
 {
     [DbContext(typeof(BichoSemDonoContext))]
-    partial class BichoSemDonoContextModelSnapshot : ModelSnapshot
+    [Migration("20220926001342_AddAdressLocalizationColumn")]
+    partial class AddAdressLocalizationColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BichoSemDono/Migrations/20220926001342_AddAdressLocalizationColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BichoSemDono.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdressLocalizationColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Localization_Address",
+                table: "OwnerlessPetPosts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Localization_Address",
+                table: "OwnerlessPetPosts");
+        }
+    }
+}
